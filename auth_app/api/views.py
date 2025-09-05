7cdc3a0c03995a069711f2c8a4aee83a5e57433e
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-09-05 16:28:10 +0200

    add CookieTokenRefreshView

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -57,15 +57,36 @@ class LogoutView(APIView):
         refresh_token = request.COOKIES.get('refresh_token')
         if refresh_token is None:
             return Response({"detail": "Refresh token not found."}, status=status.HTTP_400_BAD_REQUEST)
-            
+
         try:
             token = RefreshToken(refresh_token)
             token.blacklist()
         except TokenError:
             return Response({"detail": "Token invalid"}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         response = Response({"detail": "Log-Out successfully! All Tokens will be deleted. Refresh token is now invalid."}, status=status.HTTP_200_OK)
         response.delete_cookie('refresh_token')
         response.delete_cookie('access_token')
         return response
-        
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh_token')
+        if refresh_token is None:
+            return Response({"detail": "Refresh token not found."}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.get_serializer(data={"refresh":refresh_token})
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:
+            return Response({"detail": "Invalid refresh token."}, status=status.HTTP_401_UNAUTHORIZED)
+        access_token = serializer.validated_data.get("access")
+        response = Response({"access": "Access Token refreshed successfully."}, status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='access_token',
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax'
+        )
+        return response
