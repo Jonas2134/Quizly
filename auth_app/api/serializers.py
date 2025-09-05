30b03629014563f78e7d3917c8ce796ead39abe5
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-09-05 16:46:36 +0200

    add documentation on my views and serializers

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -6,23 +6,47 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration.
+    Validates that the username and email are unique.
+    Hashes the password before saving the user.
 
+    Fields:
+        email (str): The user's email address.
+        username (str): The user's username.
+        password (str): The user's password (write-only).
+    """
     class Meta:
+        """
+        Meta class for RegisterSerializer.
+        Specifies the model and fields to be used.
+
+        Attributes:
+            model (User): The user model.
+            fields (tuple): The fields to be included in the serializer.
+            extra_kwargs (dict): Additional keyword arguments for fields.
+        """
         model = User
         fields = ('username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate_username(self, value):
+        """Validate the username field."""
         if User.objects.filter(username=value).exists():
             raise serializers.ValidationError("Username is already taken.")
         return value
 
     def validate_email(self, value):
+        """Validate the email field."""
         if User.objects.filter(email=value).exists():
             raise serializers.ValidationError("Email is already registered.")
         return value
     
     def save(self, **kwargs):
+        """
+        Save the user instance.
+        Hashes the password before saving the user.
+        """
         pw = self.validated_data.pop('password')
         user = User(**self.validated_data)
         user.set_password(pw)
@@ -31,10 +55,16 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(TokenObtainPairSerializer):
+    """
+    Serializer for user login.
+    Validates the username and password.
+    Returns the data required for JWT authentication.
+    """
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, attrs):
+        """Validate the login credentials."""
         username = attrs.get('username')
         password = attrs.get('password')
 
@@ -51,6 +81,7 @@ class LoginSerializer(TokenObtainPairSerializer):
         return data
 
     def _check_user_exist(self, username):
+        """Return user if exists, else raise ValidationError."""
         try:
             user = User.objects.get(username=username)
             return user
@@ -58,5 +89,6 @@ class LoginSerializer(TokenObtainPairSerializer):
             raise serializers.ValidationError("User with this username does not exist.")
     
     def _check_password(self, user, password) -> None:
+        """Check if the provided password is correct."""
         if not user.check_password(password):
             raise serializers.ValidationError("Incorrect password.")
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -9,10 +9,26 @@ from .serializers import RegisterSerializer, LoginSerializer
 
 
 class RegisterView(generics.CreateAPIView):
+    """
+    API view for user registration.
+    Allows any user (authenticated or not) to access this endpoint.
+    Uses the RegisterSerializer to validate and create a new user.
+
+    Attributes:
+        serializer_class (RegisterSerializer): The serializer class to use for this view.
+        permission_classes (list): The permission classes to apply to this view.
+    """
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests for user registration.
+
+        1. Validate the incoming data using the serializer.
+        2. If valid, save the new user and return a success message with HTTP 201 status.
+        3. If invalid, return the serializer errors with HTTP 400 status
+        """
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -22,10 +38,26 @@ class RegisterView(generics.CreateAPIView):
 
 
 class LoginView(TokenObtainPairView):
+    """
+    API view for user login.
+    Allows any user (authenticated or not) to access this endpoint.
+    Uses the LoginSerializer to validate and authenticate the user.
+
+    Attributes:
+        serializer_class (LoginSerializer): The serializer class to use for this view.
+        permission_classes (list): The permission classes to apply to this view.
+    """
     serializer_class = LoginSerializer
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests for user login.
+
+        1. Validate the incoming data using the serializer.
+        2. If valid, return the access and refresh tokens in HttpOnly cookies with HTTP 200 status.
+        3. If invalid, return the serializer errors with HTTP 401 status.
+        """
         response = super().post(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
             data = response.data
@@ -51,9 +83,26 @@ class LoginView(TokenObtainPairView):
 
 
 class LogoutView(APIView):
+    """
+    API view for user logout.
+    Requires the user to be authenticated to access this endpoint.
+    Invalidates the refresh token and deletes the access and refresh tokens from cookies.
+
+    Attributes:
+        permission_classes (list): The permission classes to apply to this view.
+    """
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        """
+        Handle POST requests for user logout.
+
+        1. Retrieve the refresh token from the HttpOnly cookie.
+        2. If the token is found, blacklist it to invalidate it.
+        3. Delete the access and refresh tokens from cookies.
+        4. Return a success message with HTTP 200 status.
+        5. If the token is not found or invalid, return an error message with HTTP 400 status.
+        """
         refresh_token = request.COOKIES.get('refresh_token')
         if refresh_token is None:
             return Response({"detail": "Refresh token not found."}, status=status.HTTP_400_BAD_REQUEST)
@@ -71,7 +120,24 @@ class LogoutView(APIView):
 
 
 class CookieTokenRefreshView(TokenRefreshView):
+    """
+    API view for refreshing JWT access tokens using HttpOnly cookies.
+    Allows authenticated users to refresh their access tokens.
+    Retrieves the refresh token from an HttpOnly cookie and returns a new access token in an HttpOnly cookie.
+    Attributes:
+        permission_classes (list): The permission classes to apply to this view.
+    """
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests for refreshing the access token.
+
+        1. Retrieve the refresh token from the HttpOnly cookie.
+        2. If the token is found, validate it and generate a new access token.
+        3. Return the new access token in an HttpOnly cookie with HTTP 200 status.
+        4. If the token is not found or invalid, return an error message with HTTP 401 status.
+        """
         refresh_token = request.COOKIES.get('refresh_token')
         if refresh_token is None:
             return Response({"detail": "Refresh token not found."}, status=status.HTTP_401_UNAUTHORIZED)
